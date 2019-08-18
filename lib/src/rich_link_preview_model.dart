b523b608db2818a83cdb29c2c5b6fe3265110eee
Jairo Flores <jairofloress@gmail.com>
2019-08-18 00:11:08 -0600

    Se mejora validacion de url

--- lib/src/rich_link_preview_model.dart
+++ lib/src/rich_link_preview_model.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:open_graph_parser/open_graph_parser.dart';
-import 'package:url_launcher/url_launcher.dart';
+// import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
 import './rich_link_preview.dart';
 
 abstract class RichLinkPreviewModel extends State<RichLinkPreview> {
@@ -97,13 +98,44 @@ abstract class RichLinkPreviewModel extends State<RichLinkPreview> {
     }
   }
 
-  void _launchURL(url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
+  void _launchURL(String url) async {
+    try {
+      await launch(
+        url,
+        option: new CustomTabsOption(
+          // toolbarColor: Theme.of(context).primaryColor,
+          enableDefaultShare: true,
+          enableUrlBarHiding: true,
+          showPageTitle: true,
+          // animation: new CustomTabsAnimation.slideIn()
+          // or user defined animation.
+          animation: new CustomTabsAnimation(
+            startEnter: 'slide_up',
+            startExit: 'android:anim/fade_out',
+            endEnter: 'android:anim/fade_in',
+            endExit: 'slide_down',
+          ),
+          extraCustomTabs: <String>[
+            // ref. https://play.google.com/store/apps/details?id=org.mozilla.firefox
+            'org.mozilla.firefox',
+            // ref. https://play.google.com/store/apps/details?id=com.microsoft.emmx
+            'com.microsoft.emmx',
+          ],        
+        ),
+      );
+    } catch (e) {
+      // An exception is thrown if browser app is not installed on Android device.
+      debugPrint(e.toString());
     }
   }
+
+  // void _launchURL(url) async {
+  //   if (await canLaunch(url)) {
+  //     await launch(url);
+  //   } else {
+  //     throw 'Could not launch $url';
+  //   }
+  // }
 
   Widget buildRichLinkPreview(BuildContext context) {
     if (_ogData == null) {
